ad31090484f392d17a73dc875db1edd54e6b26ee
Isaac4Lion <isaac.leon@epn.edu.ec>
2024-07-16 21:28:58 -0500

    Autenticacion en Rutas

--- src/routers/rutas_routes.js
+++ src/routers/rutas_routes.js
@@ -8,11 +8,12 @@ import {
     eliminarRuta 
 } from '../controllers/rutas_controller.js'
 import { validacionRutas } from '../middlewares/validacionRutas.js'
+import verificarAutenticacion from '../middlewares/auth.js'
 
 router.get('/rutas',listarRutas)
 router.get('/rutas/:id',detalleRuta)
-router.post('/rutas/registro',validacionRutas,registrarRuta)
-router.put('/rutas/actualizar/:id',actualizarRuta)
-router.delete('/rutas/eliminar/:id',eliminarRuta)
+router.post('/rutas/registro',validacionRutas,verificarAutenticacion,registrarRuta)
+router.put('/rutas/actualizar/:id',verificarAutenticacion,actualizarRuta)
+router.delete('/rutas/eliminar/:id',verificarAutenticacion,eliminarRuta)
 
 export default router

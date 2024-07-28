4f4e31dd63e7ad0a1eb7a89e09cbcd932492f384
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-07-28 17:51:33 -0500

    Update ciudadano_routes.js

--- src/routers/ciudadano_routes.js
+++ src/routers/ciudadano_routes.js
@@ -16,7 +16,7 @@ const router = Router()
 router.post('/ciudadano/register',validacionCiudadano,registro)
 router.post('/ciudadano/login',login)
 router.get('/ciudadano/verify/:token',verificarToken)
-router.post('/ciudadano/recuperarcontrasena',validacionCiudadano,recuperarPassword)
+router.post('/ciudadano/recuperarcontrasena',recuperarPassword)
 router.get('/ciudadano/confirmar/:token',comprobarTokenPasword)
 router.post('/ciudadano/nuevapassword/:token',nuevoPassword)
 

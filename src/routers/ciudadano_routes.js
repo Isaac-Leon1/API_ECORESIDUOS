acc64d766a802733ce05b16826fdb602ca130738
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-07-28 17:36:37 -0500

    Update ciudadano_routes.js

--- src/routers/ciudadano_routes.js
+++ src/routers/ciudadano_routes.js
@@ -3,7 +3,10 @@ import {
     registro,
     login,
     reportarIncidente,
-    verificarToken
+    verificarToken,
+    recuperarPassword,
+    comprobarTokenPasword,
+    nuevoPassword
 } from '../controllers/ciudadano_controller.js'
 import verificarAutenticacion from '../middlewares/auth.js'
 import { validacionCiudadano } from '../middlewares/validacionCiudadanos.js'
@@ -13,6 +16,9 @@ const router = Router()
 router.post('/ciudadano/register',validacionCiudadano,registro)
 router.post('/ciudadano/login',login)
 router.get('/ciudadano/verify/:token',verificarToken)
+router.post('/ciudadano/recuperarcontrasena',validacionCiudadano,recuperarPassword)
+router.get('/ciudadano/confirmar/:token',comprobarTokenPasword)
+router.post('/ciudadano/nuevapassword/:token',nuevoPassword)
 
 router.post('/ciudadano/reports',verificarAutenticacion,reportarIncidente)
 
